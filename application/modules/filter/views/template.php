b4315b8dd0c0b03acdc3a54a000a01cbd7806471
Kemal Smayil <pento1988@gmail.com>
2012-01-15 02:43:26 +0200

    small fix

--- application/modules/filter/views/template.php
+++ application/modules/filter/views/template.php
@@ -14,7 +14,9 @@
             <div><?php echo $obj['resort']; ?></div>
             <div>
                 <!-- Превью -->
+                <?php if (isset($obj['images'][0])) : ?>
                 <img src="<?php echo base_url().'images/object/thumb/'.$obj['images'][0]; ?>" alt="" align="left"/>
+                <?php endif; ?>
                 <!-- Краткое описание -->
                 <?php echo $obj['body']; ?>
             </div>
