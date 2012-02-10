a0e0d08708f9c2b6f85990d5a28c967eba3b33e4
Kemal Smayil <pento1988@gmail.com>
2012-02-11 01:29:05 +0200

    admin design upd

--- application/modules/article/views/edit-article.php
+++ application/modules/article/views/edit-article.php
@@ -12,66 +12,80 @@
 
 <input type="hidden" name="edit-id" value="<?php echo $content['id']; ?>" />
 
-<div>
-    <div><label class="title-label">Заголовок</label></div>
-    <input type="text" name="edit-title" size="80" value="<?php echo set_value('edit-title', $content['title']) ?>"/>
-</div>
+<table><tbody><tr>
 
-<!-- Метатеги -->
-<?php print $metatags; ?>
+<td class="left-col">
 
-<div>
-    <div><label class="title-label">Место отдыха</label></div>
-    <select name="edit-resorts">
-    <option value="0"></option>
-    <?php foreach ($resorts as $r) : ?>
-    <option value="<?php echo $r['id'] ?>" <?php echo set_select('edit-resorts', $r['id'], $r['id'] == $content['resort_id'] ? TRUE : FALSE); ?> ><?php echo $r['name'] ?></option>
-    <?php endforeach; ?>
-    </select>
-</div>
-
-
-<div>
-    <div><label class="title-label">Титульное изображение</label></div>
-
-    <a href="#" id="img-remove">Удалить</a>
-
-    <input type="file" name="upload-image" />
-
-    <div id="img-container">
-        <img id="img-main" src="<?php echo base_url().'images/article/thumb/'.set_value('edit-image', $content['image_src']); ?>" alt=""/>
-    </div>
     <div>
-        <input type="button" id="img-upload" value="Загрузить" />
-        <span id="img-msg"></span>
+        <div><label class="title-label">Заголовок</label></div>
+        <input type="text" name="edit-title" value="<?php echo set_value('edit-title', $content['title']) ?>"/>
     </div>
 
-    <label>Описание</label>
-    <input type="text" name="edit-image-desc" size="30" value="<?php echo set_value('edit-image-desc', $content['image_desc']) ?>"/>
-    <input type="hidden" name="edit-image" value="<?php echo set_value('edit-image', $content['image_src']); ?>"/>
-</div>
+    <!-- Метатеги -->
+    <?php print $metatags; ?>
 
-<div>
-    <div><label class="title-label">Текст страницы</label></div>
-    <?php $this->ckeditor->editor('edit-body', html_entity_decode(set_value('edit-body', $content['body']))); ?>
-</div>
+    <div>
+        <div><label class="title-label">Место отдыха</label></div>
+        <select name="edit-resorts">
+        <option value="0"></option>
+        <?php foreach ($resorts as $r) : ?>
+        <option value="<?php echo $r['id'] ?>" <?php echo set_select('edit-resorts', $r['id'], $r['id'] == $content['resort_id'] ? TRUE : FALSE); ?> ><?php echo $r['name'] ?></option>
+        <?php endforeach; ?>
+        </select>
+    </div>
 
-<div>
-    <div><label class="title-label">Настройки публикации</label></div>
-    <label>
-        <input type="checkbox" name="edit-status" value="1" <?php echo set_checkbox('edit-status', '1', $content['status'] ? TRUE : FALSE); ?> />
-        Опубликовано
-    </label>
-    <label>
-        <input type="checkbox" name="edit-sticky" value="1" <?php echo set_checkbox('edit-sticky', '1', $content['sticky'] ? TRUE : FALSE); ?>/>
-        Закреплять вверху списка
-    </label>
-</div>
+    <div>
+        <div><label class="title-label">Настройки публикации</label></div>
+        <label>
+            <input type="checkbox" name="edit-status" value="1" <?php echo set_checkbox('edit-status', '1', $content['status'] ? TRUE : FALSE); ?> />
+            Опубликовано
+        </label>
+        <label>
+            <input type="checkbox" name="edit-sticky" value="1" <?php echo set_checkbox('edit-sticky', '1', $content['sticky'] ? TRUE : FALSE); ?>/>
+            Закреплять вверху списка
+        </label>
+    </div>
 
-<?php print $alias; ?>
+    <?php print $alias; ?>
 
-<input type="hidden" name="module-name" value="article">
+    <input type="hidden" name="module-name" value="article">
 
-<input type="submit" value="Сохранить" />
+    <input type="submit" value="Сохранить" />
+    <input type="button" value="Применить" />
+    <a href="<?php echo base_url().'admin/article' ?>">Отмена</a>
+
+</td>
+
+<td class="right-col">
+
+    <div>
+        <div><label class="title-label">Титульное изображение</label></div>
+
+        <input type="file" name="upload-image" />
+
+        <div id="img-container">
+            <a href="#" id="img-remove"></a>
+            <img id="img-main" src="<?php echo base_url().'images/article/thumb/'.set_value('edit-image', $content['image_src']); ?>" alt=""/>
+        </div>
+
+        <p><label>Описание</label></p>
+        <input type="text" name="edit-image-desc" value="<?php echo set_value('edit-image-desc', $content['image_desc']) ?>"/>
+        <input type="hidden" name="edit-image" value="<?php echo set_value('edit-image', $content['image_src']); ?>"/>
+
+        <div>
+            <input type="button" id="img-upload" value="Загрузить" />
+            <span id="img-msg"></span>
+        </div>
+
+    </div>
+
+    <div class="field-wrapper">
+        <div><label class="title-label">Текст страницы</label></div>
+        <?php $this->ckeditor->editor('edit-body', html_entity_decode(set_value('edit-body', $content['body']))); ?>
+    </div>
+
+</td>
+
+</tr></tbody></table>
 
 <?php echo form_close(); ?>
--- application/modules/metatags/views/form_fields.php
+++ application/modules/metatags/views/form_fields.php
@@ -15,10 +15,10 @@
         <input type="text" name="edit-metatitle" value="<?php echo $title; ?>" size="80" />
 
         <div>Ключевые слова</div>
-        <textarea name="edit-keywords" cols="80" rows="1" style="width: 100%;"><?php echo $keywords; ?></textarea>
+        <textarea name="edit-keywords" cols="80" rows="1" ><?php echo $keywords; ?></textarea>
         
         <div>Описание</div>
-        <textarea name="edit-desc" cols="80" rows="3" style="width: 100%;"><?php echo $desc; ?></textarea>
+        <textarea name="edit-desc" cols="80" rows="3" ><?php echo $desc; ?></textarea>
     </div>
 
     <input type="hidden" name="edit-metaid" value="<?php echo $id; ?>" />
